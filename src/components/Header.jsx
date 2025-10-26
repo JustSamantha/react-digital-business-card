f5262aab2141b0857830c57f8af312923da15077
Samantha Castillo <samanthacas@pm.me>
2025-10-26 11:04:20 -0700

    - Finished header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import profilePicture from '../assets/profile-picture.png'
+import emailIcon from '../assets/email-icon.png'
+import linkedinIcon from '../assets/linkedin-icon.png'
 
 export default function Header() {
   return (
@@ -6,7 +8,15 @@ export default function Header() {
       <img src={profilePicture} />
       <h1>Samantha Castillo</h1>
       <h2>Frontend Developer</h2>
-      <p>samanthacastillo.com</p>
+      <a href="https://www.samanthacastillo.com" alt="Samantha Castillo Website" target="_blank">samanthacastillo.com</a>
+      <div>
+        <a href="mailto:test@google.com" target="_blank" alt="Send email">
+          <button className="email-button"><img src={emailIcon} />Email</button>
+        </a>
+        <a href="https://www.linkedin.com/in/samcastillo28/" target="_blank" alt="Open LinkedIn profile">
+          <button className="linkedin-button"><img src={linkedinIcon} />LinkedIn</button>
+        </a>
+      </div>
     </header>
   )
 }
