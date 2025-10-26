961367954f1b4ea860568b25529e78651bdcdddb
Samantha Castillo <samanthacas@pm.me>
2025-10-26 11:17:52 -0700

    - Finished main content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,10 @@
 export default function Main() {
   return (
-    <main>Main</main>
+    <main>
+      <h1 className="about-title">About</h1>
+      <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+      <h1>Interests</h1>
+      <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </main>
   )
 }
